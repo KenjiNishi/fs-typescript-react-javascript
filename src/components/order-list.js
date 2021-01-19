ceabc1e21b43eaf3a5f2f68003c9fa593ae56f55
KenjiNishi <wk.nishizawa@gmail.com>
2021-01-19 17:59:19 -0300

    showing items on orderList

--- src/components/order-list.js
+++ src/components/order-list.js
@@ -2,76 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 
 import { fetchOrders } from '../redux/actions/orderActions';
 
-const Order = props => (
-    <Card style={{ width: '20rem' }} border={props.setBorder(props.order.situacao)}>
-      <Card.Body>
-          <Container>
-              <Row>
-                  <Col>
-                    <p>Pedido {props.order.numero} ({props.setSituacao(props.order.situacao)})</p>
-                  </Col>
-                  <Col> 
-                    <p>{props.order.data.substring(0,10)}</p>
-                  </Col>
-                </Row>
+const Order = props => {
+    var cardBorder = (status) =>{switch(status){
+        case 0:
+            return "dark";
+        case 1:
+            return "success";
+        case -1:
+            return "danger";
+        default:
+            return "info";
+    }};
 
-                <Row>
-                    Descrição: {props.order.descricao}
-                </Row>
-
-                <Row>
-                    ITENS 
-                </Row>
-
-                <Row>
-                    <button onClick={() => { 
-                        props.fetchOrders()
-                    }}>Aprovar</button> 
-
-                    <button onClick={() => { 
-                        props.fetchOrders()
-                    }}>Cancelar</button>
-                </Row>
-          </Container> 
-      </Card.Body>
-      
-  </Card>
-)
-
-class OrdersList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.setCardBorder = this.setCardBorder.bind(this);
-
-        this.state = {
-
-        }
-    }
-
-    componentDidMount() {
-        this.props.fetchOrders();
-    }
-
-    setCardBorder(status){
-        switch(status){
-            case 0:
-                return "dark";
-            case 1:
-                return "success";
-            case -1:
-                return "danger";
-            default:
-                return "info";
-        }
-    }
-
-    setSituacao(status){
-        switch(status){
+    var cardStatus = (status) =>{switch(status)
+        {
             case 0:
                 return "Em Análise";
             case 1:
@@ -81,20 +29,97 @@ class OrdersList extends Component {
             default:
                 return "no-status";
         }
+    };
+
+    return(
+        <Card style={{ width: '40rem' }} border={cardBorder(props.order.situacao)}>
+        <Card.Body>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>Pedido {props.order.numero} ( {cardStatus(props.order.situacao)} )</p>
+                    </Col>
+                    <Col> 
+                        <p>{props.order.data.substring(0,10)}</p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    Descrição: {props.order.descricao}
+                </Row>
+
+                <Row>
+                    <ListGroup>
+                        {props.orderItemList(props.order.itens)}
+                    </ListGroup>
+                </Row>
+
+                <Row>
+                    <button onClick={() => { 
+                        props.fetchOrders()
+                    }}>Aprovar</button> 
+                    <button onClick={() => { 
+                        props.fetchOrders()
+                    }}>Cancelar</button>
+                </Row>
+            </Container> 
+        </Card.Body>
+    </Card>
+    )
+}
+
+const OrderItem = props => 
+{
+    return(
+        <ListGroup.Item>
+            <Container>
+                <Row>
+                    {props.item.codigo} 
+                </Row>
+                <Row>
+                    <Col>
+                        Quantidade: {props.item.quantidade}
+                    </Col>
+                    <Col>
+                        Preço: {props.item.valorUnitario} Desconto: {props.item.desconto}%
+                    </Col>
+                    <Col>
+                        Total: {props.item.valorTotal}
+                    </Col>
+                </Row>
+            </Container>
+        </ListGroup.Item>
+    )
+}
+
+class OrdersList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+        this.props.fetchOrders();
     }
 
     orderList() {
         return this.props.orders.map(currentorder => {
-        return <Order order={currentorder} fetchOrders={this.props.fetchOrders} setSituacao={this.setSituacao} setBorder={this.setCardBorder} key={currentorder.numero}/>;
+            return <Order order={currentorder} fetchOrders={this.props.fetchOrders} orderItemList={this.orderItemList} key={currentorder.numero}/>;
         })
     }
 
+    orderItemList(items) {
+        return items.map(current => {
+            return <OrderItem item={current} key={current.codigo}/>;
+        })
+    }
 
     render() {
         return (
             <Container>
                 <Row>
-                    <h3>Pedidos registrados:</h3>
+                    <h1>Pedidos registrados:</h1>
                 </Row>
                 <Row>
                     { this.orderList() }
