204f8a093542f446b883cb0a52758b1b00213b0a
KenjiNishi <wk.nishizawa@gmail.com>
2021-01-19 14:53:46 -0300

    connected ordeReducers to  redux store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import itemReducer from './itemReducers';
+import orderReducer from './orderReducers';
 
 export default combineReducers({
-  items: itemReducer //state.items.VAR  eg VAR=itemList
+  items: itemReducer, //state.items.VAR  eg VAR=itemList
+  orders: orderReducer
 });
