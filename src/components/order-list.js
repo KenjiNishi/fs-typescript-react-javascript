ec53e0f178be7da569bbd4e8be101a6d0ff6cd04
KenjiNishi <wk.nishizawa@gmail.com>
2021-01-19 16:47:46 -0300

    rustic card display for orders

--- src/components/order-list.js
+++ src/components/order-list.js
@@ -1,11 +1,89 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class OrdersList extends Component {
-    render() {
-        return (
-            <div className='container'>
-                <p>Hello there</p>
-            </div>
-        )
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+import { fetchOrders } from '../redux/actions/orderActions';
+
+const Order = props => (
+  <Card style={{ width: '20rem' }}>
+      <Card.Body>
+          <Container>
+              <Row>
+                  <Col>
+                    <p>Pedido {props.order.numero}</p>
+                  </Col>
+                  <Col> 
+                    <p>{props.order.data.substring(0,10)}</p>
+                  </Col>
+                </Row>
+
+                <Row>
+                    Descrição: {props.order.descricao}
+                </Row>
+
+                <Row>
+                    ITENS
+                </Row>
+
+                <Row>
+                    <button onClick={() => { 
+                        props.fetchOrders()
+                    }}>Aprovar</button> 
+
+                    <button onClick={() => { 
+                        props.fetchOrders()
+                    }}>Cancelar</button>
+                </Row>
+          </Container> 
+      </Card.Body>
+      
+  </Card>
+)
+
+class OrdersList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
     }
+  }
+
+  componentDidMount() {
+    this.props.fetchOrders();
+  }
+
+
+  orderList() {
+    return this.props.orders.map(currentorder => {
+      return <Order order={currentorder} fetchOrders={this.props.fetchOrders} key={currentorder.numero}/>;
+    })
+  }
+
+
+  render() {
+    return (
+        <Container>
+            <Row>
+                <h3>Pedidos registrados:</h3>
+            </Row>
+            <Row>
+                { this.orderList() }
+            </Row>
+        </Container>
+    )
+  }
 }
+
+OrdersList.propTypes = {
+  fetchOrders: PropTypes.func.isRequired,
+  orders: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => (
+  {
+  orders: state.orders.orderList
+});
+
+export default connect(mapStateToProps, { fetchOrders})(OrdersList);
